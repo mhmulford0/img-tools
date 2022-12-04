fc3eeaf2111483a4ca7b3489b8165e44ec0b0292
Matthew Mulford <mhmulford0@gmail.com>
2022-12-04 16:20:03 -0600

    misc

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,43 @@ import { join } from 'path';
 import sharp from 'sharp';
 import { createReadStream } from 'fs';
 
-export const queue = new Queue('resize-img');
+export const queue = new Queue('resize-img', {
+  connection: {
+    host: '0.0.0.0',
+    port: 6379,
+  },
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '4mb' }));
   app.use(urlencoded({ extended: true, limit: '4mb' }));
 
-  new Worker('resize-img', async (job: Job) => {
-    console.log(job.data);
-    try {
-      const imgBuffer = Buffer.from(job.data.imgData.data, 'utf-8');
-      await sharp(imgBuffer)
-        .resize({ width: job.data.width, height: job.data.height })
-        .toFile(`tmp/${job.data.tmpName}.${job.data.mimeType}`);
+  new Worker(
+    'resize-img',
+    async (job: Job) => {
+      try {
+        const imgBuffer = Buffer.from(job.data.imgData.data, 'utf-8');
+        await sharp(imgBuffer)
+          .resize({ width: job.data.width, height: job.data.height })
+          .toFile(`tmp/${job.data.tmpName}.${job.data.mimeType}`);
 
-      const fileStream = createReadStream(
-        join(process.cwd(), `tmp/${job.data.tmpName}.${job.data.mimeType}`),
-      );
+        const fileStream = createReadStream(
+          join(process.cwd(), `tmp/${job.data.tmpName}.${job.data.mimeType}`),
+        );
 
-      return fileStream;
-    } catch (error) {
-      console.error(error);
-    }
-  });
+        return fileStream;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    {
+      connection: {
+        host: '0.0.0.0',
+        port: 6379,
+      },
+    },
+  );
 
   await app.listen(process.env.PORT || 3000);
 }
